aeffee3800ef74867a72cb63a73b21bef8f322b2
steven chareonbood <stoodsnob@gmail.com>
2019-07-24 17:32:24 +0800

    d

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant1 = Restaurant.create(name: "Crazy House", address: "Street 1", category: "chinese")
+Restaurant2 = Restaurant.create(name: "Frank the Tank", address: "Street 2", category: "italian")
+Restaurant3 = Restaurant.create(name: "Rock House", address: "Street 3", category: "japanese")
+Restaurant4 = Restaurant.create(name: "Fragomaat", address: "Street 4", category: "french")
+Restaurant5 = Restaurant.create(name: "Frauen Haus", address: "Street 5", category: "belgian")
